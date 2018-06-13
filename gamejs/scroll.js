0fe7cd17c26199e268312700f8a6f40d39295a4b
Kertu Kipper <phkel@users.noreply.github.com>
2018-06-13 11:55:35 +0300

    Add target blank

--- gamejs/scroll.js
+++ gamejs/scroll.js
@@ -14,11 +14,11 @@ $(document).ready(function () {
 
   function onScroll(event){
     var scrollPos = $(document).scrollTop() +50;
-    $('.menu a').each(function () {
+    $('.menu menuLink').each(function () {
       var currLink = $(this);
       var refElement = $(currLink.attr("href"));
       if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-        $('.menu a').removeClass("active");
+        $('.menu menuLink').removeClass("active");
         currLink.addClass("active");
       }
       else{
