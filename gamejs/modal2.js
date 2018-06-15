da0ef273a15bd0f22fa499f1942858d6d35693a7
Kertu Kipper <phkel@users.noreply.github.com>
2018-06-15 12:58:46 +0300

    Fix programme modal

--- gamejs/modal2.js
+++ gamejs/modal2.js
@@ -1,12 +1,20 @@
-var modal_2 = document.getElementById("imgModal_2");
-var img = document.getElementById("short");
-var modalImg = document.getElementById("full");
-var span = document.getElementsByClassName("exit")[0]
+// Get the modal
+var modal = document.getElementById('myModal');
 
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption_2");
 img.onclick = function(){
-	modal_2.style.display = "block";
-	modalImg.src = "img/scheme.png";
+    modal.style.display = "block";
+    modalImg.src = "img/scheme.png";
+    captionText.innerHTML = this.alt;
 }
-span.onclick = function(){
-	modal_2.style.display = "none";
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close_2")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
 }
